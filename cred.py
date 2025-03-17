1bdf476bf0d0a06061ea909e871193c900ddcd4a
Mauro Moreno <Mauro>
2025-03-17 13:32:58 -0300

    Input constante, leitura por CPF.

--- cred.py
+++ cred.py
@@ -1,9 +1,26 @@
 import openpyxl as xl
+import re as re
 
-arquivo = xl.load_workbook(filename="C:/Users/mamfy/Desktop/prog/proj03-CRED/teste.xlsx")
+arquivo = xl.load_workbook(filename="C:/Users/mamfy/Desktop/prog/proj03-CRED/exemplo_dados_com_cpf.xlsx")
 
 ws = arquivo.active
 
-for row in ws.iter_rows():
-    if row[0].value.find("João") != -1:
-        print([cell.value for cell in row])
+def pesquisa(query):
+
+    query = re.sub(r'\D', '', query)
+
+    for row in ws.iter_rows(values_only=True): 
+        if re.sub(r'\D', '', str(row[0])) == query:
+            print(row)
+            return
+    print("404")
+
+while True:
+    try:
+        query = input("Digite o nome para pesquisa: ")
+        pesquisa(query)
+        if query.lower() == 'sair':
+            break
+    except EOFError:
+        break
+
