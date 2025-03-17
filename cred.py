2f1750f5d5f42eb239619f27cb00f3949a9d12dd
Mauro Moreno <Mauro>
2025-03-17 17:23:43 -0300

    Pesquisa por RG, marcar Presença

--- cred.py
+++ cred.py
@@ -5,20 +5,39 @@ arquivo = xl.load_workbook(filename="C:/Users/mamfy/Desktop/prog/proj03-CRED/exe
 
 ws = arquivo.active
 
-def pesquisa(query):
+def addPresenca():
+
+    if ws.cell(row=1, column=ws.max_column).value != "PRESENÇA":
+        ws.insert_cols(ws.max_column + 1)
+        ws.cell(row=1,column=ws.max_column+1,value="PRESENÇA")
+
+        for row in ws.iter_rows(min_row=2, max_row=ws.max_row):
+            row[-1].value = "AUSENTE"
+    
+    arquivo.save("C:/Users/mamfy/Desktop/prog/proj03-CRED/exemplo_dados_com_cpf_teste.xlsx")
+
+addPresenca()
+
+def pesquisa(query, r):
 
     query = re.sub(r'\D', '', query)
 
-    for row in ws.iter_rows(values_only=True): 
-        if re.sub(r'\D', '', str(row[0])) == query:
-            print(row)
+    for row in ws.iter_rows(values_only=False): 
+        if re.sub(r'\D', '', str(row[r].value)) == query:
+            row[-1].value = "PRESENTE"
+            arquivo.save("C:/Users/mamfy/Desktop/prog/proj03-CRED/exemplo_dados_com_cpf_teste.xlsx")
+            print([cell.value for cell in row])
             return
     print("404")
 
 while True:
     try:
-        query = input("Digite o nome para pesquisa: ")
-        pesquisa(query)
+        query = input("Digite o CPF / RG para pesquisa: ")
+        #pesquisa(query)
+        if len(query) < 11:
+            pesquisa(query, 1)
+        else: pesquisa(query, 0)
+
         if query.lower() == 'sair':
             break
     except EOFError:
